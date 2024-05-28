665f68a4cbc98a0a662e88a7422178b15029f4d7
arosteguimaranto <arosteguimaranto@outlook.com>
2024-05-28 12:34:51 -0600

    Using Template List in Storybook

--- stories/Card/Card.stories.js
+++ stories/Card/Card.stories.js
@@ -1,32 +1,32 @@
-
 import Card from "./Card";
 import { options } from "./constants";
 
 export default {
-    title: "Card",
-    component: Card,
+  title: "Card",
+  component: Card,
 };
 
-export const Template = (args) => <Card {...args}/>
+export const Template = (args) => <Card {...args} />;
+const ListTemplate = ({ items, ...args }) =>
+  items.map((item, index) => <Card key={index} {...args} {...item} />);
 
 export const Default = Template.bind({});
 
-export const Clickable = Template.bind({})
+export const Clickable = Template.bind({});
 Clickable.args = {
-    isClickable: true
+  isClickable: true,
 };
-export const Dragable =  Template.bind({})
+export const Dragable = Template.bind({});
 Dragable.args = {
-    isDragable: true
-}
-;
+  isDragable: true,
+};
 
-export const Colors = () => 
-    options.colors.map((color, index) => {
-        return <Card key={index} color={color}/>
-    });
+export const Colors = ListTemplate.bind({});
+Colors.args = {
+  items: options.colors.map((color) => ({ color })),
+};
 
-export const Sizes = () => 
-    options.sizes.map((size, index) =>{
-        return <Card key={index} size={size}/>
-    });
+export const Sizes = ListTemplate.bind({});
+Sizes.args = {
+  items: options.sizes.map((size) => ({ size })),
+};
